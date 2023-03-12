f8082ed7f777da2918d63bf97fd2f2dfe01195ca
ShamanEvrei <andres1999111@gmail.com>
2023-03-12 22:37:30 +0700

    Homework1

---
+++ HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HomeWork1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ваше имя?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name} !");
+
+        }
+    }
+}
+
